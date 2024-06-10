777cadb6e41214027d8390ffe6af9456bf1a29ce
Pymode-Dev <pymode000@gmail.com>
2024-06-10 15:50:43 +0100

    wc completed but not fully

--- wc/main.py
+++ wc/main.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python3
 
+import re
 import sys
+
+from argparse import ArgumentParser, FileType
 from pathlib import Path
 
-from wc import wc_all, wc_byte, wc_word, wc_line, wc_stdin, wc_char
+from wc import wc_all, wc_byte, wc_word, wc_line, wc_char
 
 
-def get_option():
-    option = sys.argv[1] if len(sys.argv) > 2 else ''
-    return option
+def parser():
+    args = ArgumentParser(
+        prog="wc",
+        description="count words, lines, characters, and bytes in a file",
+        epilog="Thanks for using wc"
+    )
+    args.add_argument("file", type=FileType('r'), default=sys.stdin, nargs="?")
+    args.add_argument("-l", help="Count lines in a file", action='store_true')
+    args.add_argument("-c", help="Count bytes in a file", action='store_true')
+    args.add_argument("-m", help="Count characters in a file", action='store_true')
+    args.add_argument("-w", help="Count words in a file", action='store_true')
+    args.add_argument("-v", action="version", version="%(prog)s 1.0")
 
-
-def get_filepath():
-    filepath = ''
-
-    if len(sys.argv) > 2:
-        filepath = Path(sys.argv[2])
-    elif len(sys.argv) == 3:
-        filepath = sys.argv[1]
-    else:
-        filepath = None
-    return filepath
-
-
-OPTION = get_option()
-FILE_PATH = get_filepath()
+    return args.parse_args()
 
 
 options = {
@@ -37,11 +35,19 @@ options = {
 
 
 def execute():
-    if OPTION and FILE_PATH:
-        print(options[OPTION](FILE_PATH))
-    else:
-        wc_stdin(options[OPTION])
+    args = parser()
+    funct = args.__dict__.items()
+    opt = [k for k, v in funct if v is True]
+    option = f"-{opt[0]}" if opt != [] else ''
 
+    file = args.file.name
+
+    if file == '<stdin>':
+        print(options[option](sys.stdin.read().encode()))
+    else:
+        print(options[option](file))
+
+    
 
 if __name__ == '__main__':
     execute()
--- wc/wc.py
+++ wc/wc.py
@@ -7,37 +7,50 @@ import sys
 from pathlib import Path
 
 
+def read_file(filepath, mode: str, line=False, e=None):
+    try:
+        with open(filepath, mode=mode, encoding=e) as f:
+            if line:
+                file_reader = f.readlines()
+            else:
+                file_reader = f.read()
+            return file_reader
+    except FileNotFoundError:
+        print(f"[FileNotFoundError] the file {filepath} does not exist.")
+        sys.exit(0)
+
+
 def wc_byte(filepath: Path) -> int:
-    with open(filepath, mode="rb") as f:
-        file_reader = f.read()
-        return len(file_reader)
+    if isinstance(filepath, bytes):
+        return len(filepath)
+    output = read_file(filepath,"rb")
+    return len(output)
 
 
 def wc_line(filepath: Path) -> int:
-    with open(filepath, mode="rb") as f:
-        file_reader = f.readlines()
-        return len(file_reader)
+    if isinstance(filepath, bytes):
+        return len(filepath.decode().strip().split('\n'))
+    else:
+        file_reader = read_file(filepath, "rb", True)
+    return len(file_reader)
 
 
 def wc_char(filepath: Path) -> int:
-    with open(filepath, mode="rb") as f:
-        file_reader = f.read()
-        return len(file_reader)
+    if isinstance(filepath, bytes):
+        return len("".join(filepath.decode().split('\r\n\" "')))
+    file_reader = read_file(filepath, "r", e="utf-8")
+    return sum([len(i) for i in file_reader.split("\r\n")])
 
 
 def wc_word(filepath: Path) -> int:
-    with open(filepath, mode="rb") as f:
-        file_reader = f.read().split()
-        return len(file_reader)
+    if isinstance(filepath, bytes):
+        return len(filepath.decode().split())
+    file_reader = read_file(filepath, "r", e="utf-8")
+    return len(file_reader.split())
 
 
 def wc_all(filepath: Path):
     byte_read = wc_byte(filepath)
     line_read = wc_line(filepath)
     word_read = wc_word(filepath)
-    return f"{line_read} {word_read} {byte_read} {filepath}"
-
-
-def wc_stdin(func):
-    for line in sys.stdin:
-        return func(line.strip())
+    return (f"{line_read} {word_read} {byte_read} {'' if isinstance(filepath, bytes) else filepath}")
